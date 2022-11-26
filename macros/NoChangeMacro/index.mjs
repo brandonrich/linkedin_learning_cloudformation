841849c0af78085baea3d5cbddd86805a2cab77e
Brandon Rich <brich@nd.edu>
2022-11-25 19:32:37 -0500

    initial revision

---
+++ macros/NoChangeMacro/index.mjs
@@ -0,0 +1,22 @@
+export const handler = async(event) => {
+    
+    
+    // var region = event.region
+    // var accountID = event.accountID
+    // var fragment = event.fragment
+    // var transformId = event.transformId
+    // var params = event.params
+    // var requestID = event.requestId
+    // var templateParameterValues = event.templateParameterValues
+    
+    console.log("Hello from lambda!")
+    console.log( "event params: " + JSON.stringify(event, 2, null))
+
+    // TODO implement
+    const response = {
+        requestId: event.requestId,
+        status: "success",
+        fragment: event.fragment // no change
+    };
+    return response;
+};
